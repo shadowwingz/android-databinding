cd964f9f1ef092e01e880e654d946c2a34962756
shadowwingz <shadowwingzhang@gmail.com>
2022-06-01 13:47:03 +0800

    DataBinding 初体验

--- app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
@@ -27,10 +27,12 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.widget.ImageViewCompat
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.databinding.basicsample.R
 import com.example.android.databinding.basicsample.data.Popularity
 import com.example.android.databinding.basicsample.data.SimpleViewModel
+import com.example.android.databinding.basicsample.databinding.PlainActivityBinding
 
 /**
  * Plain old activity with lots of problems to fix.
@@ -43,10 +45,13 @@ class PlainOldActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.plain_activity)
+        // setContentView(R.layout.plain_activity)
+      val binding: PlainActivityBinding = DataBindingUtil.setContentView(this, R.layout.plain_activity)
 
-        // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
-        updateName()
+      binding.name = viewModel.name
+      binding.lastName = viewModel.lastName
+      // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
+      //   updateName()
         updateLikes()
     }
 
